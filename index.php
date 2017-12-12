48c269d7b46d528d58b6a50d2c88199e502ee81f
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 11:42:22 +0100

    ajout exo3

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
-$age = 25;
-$genre = 'homme';
-
-if ($age >= 18 && $genre == 'homme') {
-  echo ('Vous êtes un homme et vous êtes majeur');
-}
-elseif ($age < 18 && $genre == 'homme') {
-  echo ('Vous êtes un homme et vous êtes mineur');
-}
-elseif ($age < 18 && $genre == 'femme') {
-  echo ('Vous êtes une femme et vous êtes mineur');
-}
-elseif ($age >= 18 && $genre == 'femme') {
-  echo ('Vous êtes une femme et vous êtes majeur');
+$numberOne = 100;
+$numberTwo = 6;
+while ($numberOne >=10) {
+  echo ($numberTwo * $numberOne.' ');
+  $numberOne --;
 }
 ?>
