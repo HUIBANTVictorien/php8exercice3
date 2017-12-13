3c2b19191a65ec76d5bc8ad59983b1c7009a2400
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-13 09:45:41 +0100

    ajout exo3

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<?php
-$numberOne = 100;
-$numberTwo = 6;
-while ($numberOne >=10) {
-  echo ($numberTwo * $numberOne.' ');
-  $numberOne --;
-}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 3 partie 7 php</title>
+  </head>
+  <body>
+    <form class="form" action="user.php" method="get">
+      <input type="lastname" name="lastname" placeholder="Nom">
+      <input type="firstname" name="firstname" placeholder="Prénom">
+      <button type="submit" name="validate">Valider</button>
+    </form>
+    <?php
+
+    ?>
+  </body>
+</html>
