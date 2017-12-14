bc05660d6ac70860a70102f76d33f4618d99351e
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-14 08:46:02 +0100

    ajout exo3

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
+<?php
+if (isset($_POST['pseudo']) && ($_POST['password'])) {
+  setcookie('pseudo', $_POST['pseudo'], time() + 365*24*3600, '/', null, false, true);
+  setcookie('password', $_POST['password'], time() + 365*24*3600, '/', null, false, true);
+}
+?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>exercice 3 partie 7 php</title>
-  </head>
-  <body>
-    <form class="form" action="user.php" method="get">
-      <input type="lastname" name="lastname" placeholder="Nom">
-      <input type="firstname" name="firstname" placeholder="Prénom">
-      <button type="submit" name="validate">Valider</button>
-    </form>
+<head>
+  <meta charset="utf-8">
+  <title>exercice 3 partie 8 php</title>
+</head>
+<body>
+  <form class="form" action="index.php" method="post">
+    <input type="text" name="pseudo" placeholder="pseudo">
+    <input type="password" name="password" placeholder="mot de passe">
+    <button type="submit" name="validate">Valider</button>
+  </form>
+  <p>
     <?php
-
+    echo 'Bonjour ' . htmlspecialchars($_COOKIE['pseudo']) . htmlspecialchars($_COOKIE['password']);
     ?>
-  </body>
+  </p>
+</body>
 </html>
